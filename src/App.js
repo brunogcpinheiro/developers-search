7c717dcaff6c4e2a0c205ff55b55f15e6faace31
Bruno Pinheiro <brunogcpinheiro@hotmail.com>
2019-01-18 15:49:54 +0000

    intial commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, { Fragment } from "react";
+import { Provider } from "react-redux";
+
+import "./config/reactotron";
+import store from "./store";
+
+import { GlobalStyle } from './styles/global';
+import Routes from './routes';
+
+const App = () => (
+  <Provider store={store}>
+    <Fragment>
+      <GlobalStyle />
+      <Routes />
+    </Fragment>
+  </Provider>
+);
+
+export default App;
